018967f3fb153fc954e1d23d957d196e0c1eaa16
igor-miranda <igor.miranda@ufvjm.edu.br>
2023-02-22 17:19:33 -0300

    Cliente Servidor

---
+++ Cliente/Cliente/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string serverAddress = "localhost";
+        int serverPort = 8080;
+        var webSocket = new ClientWebSocket();
+
+        // Verifica se o servidor está ativo.
+        try
+        {
+            // cria o objeto TcpClient e tenta se conectar ao servidor
+            using (var tcpClient = new TcpClient())
+            {
+                tcpClient.Connect(serverAddress, serverPort);
+
+                // se a conexão for bem sucedida, o servidor está ativo
+                Console.WriteLine("O servidor está ativo.");
+                await webSocket.ConnectAsync(new Uri("ws://localhost:8080"), CancellationToken.None);
+
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    Console.WriteLine("Envie uma mensagem para o Servidor: ");
+                    var message = Console.ReadLine();
+                    var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                    await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                    //Recebe responsa do servidor.
+                    var bufferServidor = new ArraySegment<byte>(new byte[4096]);
+                    var result = await webSocket.ReceiveAsync(bufferServidor, CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        var messageServidor = System.Text.Encoding.UTF8.GetString(bufferServidor.Array, bufferServidor.Offset, result.Count);
+                        Console.WriteLine($"Menssagem recebida do servidor: {messageServidor}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            // se ocorrer uma exceção, o servidor não está ativo
+            Console.WriteLine("Não foi possível conectar ao servidor. Erro: " + ex.Message);
+        }
+        finally
+        {
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+        }
+    }
+}
---
+++ Servidor/Servidor/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/");
+        listener.Start();
+
+        while (true)
+        {
+            var context = await listener.GetContextAsync();
+            if (IsWebSocketRequest(context))
+            {
+                try
+                {
+                    var webSocketContext = await context.AcceptWebSocketAsync(null);
+                    var webSocket = webSocketContext.WebSocket;
+                    await Program.HandleWebSocketConnectionAsync(webSocket);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"WebSocket error: {ex.Message}");
+                }
+            }
+
+        }
+    }
+
+    static bool IsWebSocketRequest(HttpListenerContext context)
+    {
+        return context.Request.Headers["Upgrade"] == "websocket"
+            && context.Request.Headers["Connection"]?.Contains("Upgrade") == true;
+    }
+
+    static async Task HandleWebSocketConnectionAsync(WebSocket webSocket)
+    {
+        try
+        {
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var buffer = new ArraySegment<byte>(new byte[4096]);
+                var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                }
+                else if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    var message = System.Text.Encoding.UTF8.GetString(buffer.Array, buffer.Offset, result.Count);
+
+                    var responseMessage = "Entregue!";
+                    // converte a mensagem de resposta em um array de bytes
+                    var responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+
+                    // envia a mensagem de resposta de volta para o cliente
+                    await webSocket.SendAsync(responseBytes, WebSocketMessageType.Text, true, CancellationToken.None);
+                    Console.WriteLine($"Menssagem recebida do Cliente: {message}");
+
+                    if (message == "close WebSocket")
+                        break;
+                }
+            }
+        }
+        catch (WebSocketException ex)
+        {
+            Console.WriteLine($"WebSocket error: {ex.Message}");
+        }
+        finally
+        {
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+        }
+    }
+}
